87fb8032bccfc01a402a6a9fca07330f27b48f61
yanhua.chen <yanhua.chen@bkjk.com>
2018-07-05 10:32:01 +0800

    在 SpringBoot 环境中，可以不加 @EnableTransactionManagement 注解 因为，SpringBoot 会自动注入 DataSourceTransactionManagerAutoConfiguration 有了此类，就会注入 DataSourceTransactionManager

--- spring-boot-06-data-mybatis/src/main/java/com/atguigu/springboot/SpringBoot06DataMybatisApplication.java
+++ spring-boot-06-data-mybatis/src/main/java/com/atguigu/springboot/SpringBoot06DataMybatisApplication.java
@@ -3,10 +3,8 @@ package com.atguigu.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan(value = "com.atguigu.springboot.mapper")
-@EnableTransactionManagement
 @SpringBootApplication
 public class SpringBoot06DataMybatisApplication {
 
@@ -14,3 +12,14 @@ public class SpringBoot06DataMybatisApplication {
         SpringApplication.run(SpringBoot06DataMybatisApplication.class, args);
     }
 }
+
+/**
+ * @EnableTransactionManagement
+ *
+ * 基本上等同于 <tx:annotation-driven/>
+ * 但是，在 SpringBoot 环境中，也可以不加此注解！！
+ * 因为，SpringBoot 会自动注入 DataSourceTransactionManagerAutoConfiguration
+ * 有了此类，就会注入 DataSourceTransactionManager
+ *
+ * 但是，如果是在非 SpringBoot 环境中，也想要使用纯注解的方式来使用事务的话，就需要加上 @EnableTransactionManagement 注解
+ */
