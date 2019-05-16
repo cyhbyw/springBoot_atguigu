7815386b34c2d92100a8ba064aa53f654960737f
yanhua.chen <yanhua.chen@bkjk.com>
2019-05-16 23:25:33 +0800

    Web mvc config: 继承了WebMvcConfigurationSupport之后，再配置"yyyy-MM-dd HH:mm:ss"这种日期格式

--- spring-boot-cyh-04-webmvc-messageConverter/src/main/java/com/cyh/web/mvc/message/converter/config/MessageConvertConfiguration.java
+++ spring-boot-cyh-04-webmvc-messageConverter/src/main/java/com/cyh/web/mvc/message/converter/config/MessageConvertConfiguration.java
@@ -1,13 +1,25 @@
 package com.cyh.web.mvc.message.converter.config;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 
 /**
  * 如果有配置文件继承了 DelegatingWebMvcConfiguration 或者 WebMvcConfigurationSupport
@@ -22,17 +34,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Configuration
 public class MessageConvertConfiguration extends WebMvcConfigurationSupport {
 
+    private static final String DATE_FORMAT_DATETIME = "yyyy-MM-dd HH:mm:ss";
+
     @Autowired
     private HttpMessageConverters httpMessageConverters;
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        System.err.println("-->  configureMessageConverters");
         super.configureMessageConverters(converters);
         /*
          * "birthday": "2019-05-10T02:59:05.588Z"
          * 这样写了之后，需要按照上面的格式，否则会报错
          * 这种格式，应该也是默认的格式，不常用
-         * 所以，一般的项目代码需要自定义一种格式，比如最常用的 yyyy-MM-dd hh:mm:ss
+         * 
+         * 所以，一般的项目代码需要自定义一种格式，比如最常用的 yyyy-MM-dd HH:mm:ss
+         * 如下面的 MyJackson2ObjectMapperBuilderCustomizer 所示
          */
         converters.addAll(httpMessageConverters.getConverters());
     }
@@ -49,6 +66,44 @@ public class MessageConvertConfiguration extends WebMvcConfigurationSupport {
     public void addInterceptors(InterceptorRegistry registry) {
         super.addInterceptors(registry);
         registry.addInterceptor(new DateFormatInterceptor()).addPathPatterns("/**");
+    }
+
+
+    /**
+     * 由于继承了 WebMvcConfigurationSupport 所以需要这样写才能生效
+     ************** 不知道有没有其它更好的办法 *******************
+     */
+    @Bean
+    public MyJackson2ObjectMapperBuilderCustomizer platformJackson2ObjectMapperBuilderCustomizer(
+            JacksonProperties jacksonProperties) {
+        return new MyJackson2ObjectMapperBuilderCustomizer(jacksonProperties);
+    }
+
+    public static class MyJackson2ObjectMapperBuilderCustomizer
+            implements Jackson2ObjectMapperBuilderCustomizer, Ordered {
+
+        private final JacksonProperties jacksonProperties;
+
+        public MyJackson2ObjectMapperBuilderCustomizer(final JacksonProperties jacksonProperties) {
+            this.jacksonProperties = jacksonProperties;
+        }
+
+        @Override
+        public void customize(final Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
+            System.err.println("-->  Jackson2ObjectMapperBuilderCustomizer.customize");
+            jacksonObjectMapperBuilder.annotationIntrospector(new JacksonAnnotationIntrospector());
+            jacksonProperties.getDeserialization().put(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            jacksonProperties.getMapper().put(MapperFeature.USE_WRAPPER_NAME_AS_PROPERTY_NAME, true);
+            jacksonProperties.setDateFormat(!StringUtils.isEmpty(jacksonProperties.getDateFormat())
+                    ? jacksonProperties.getDateFormat() : DATE_FORMAT_DATETIME);
+            jacksonProperties.setTimeZone(Objects.nonNull(jacksonProperties.getTimeZone())
+                    ? jacksonProperties.getTimeZone() : TimeZone.getDefault());
+        }
+
+        @Override
+        public int getOrder() {
+            return HIGHEST_PRECEDENCE;
+        }
     }
 
 }
